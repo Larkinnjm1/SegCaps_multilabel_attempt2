829888c8b237c2bdb53ca92362b8e69f1654940c
Niall Larkin <niall.larkin@city.ac.uk>
2019-10-28 15:12:31 +0000

    Removal of experiments folder

--- main_train_and_test.py
+++ main_train_and_test.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
                 'num_labels':5,'learning_rates':0.0003,
                 'data_format':'channels_first',
                 'save_debug_images':False,'image_size':[256,256], #5000,0.001
-                'aug_dict_path':'./aug_dict_dual.json','patience':249,'earlystop_sp':0.5,
+                'aug_dict_path':'./aug_dict_prob.json','patience':249,'earlystop_sp':0.5,
                 'class_weights_arr':np.array([0.03987201, 0.36867433, 0.35872208, 0.2314718 , 0.00125978])},
 {'loss_function':weighted_spread_loss,'network':SegCaps_multilabels,
             'routing_type':'','batch_size':1,'max_iter':7500,
@@ -219,7 +219,7 @@ if __name__ == '__main__':
             'num_labels':5,'learning_rates':0.001,
             'data_format':'channels_first',
             'save_debug_images':False,'image_size':[256,256],
-            'aug_dict_path':'./aug_dict_dual.json','patience':5000,'earlystop_sp':0.001,
+            'aug_dict_path':'./aug_dict_prob.json','patience':5000,'earlystop_sp':0.001,
             'class_weights_arr':np.array([0.03987201, 0.36867433, 0.35872208, 0.2314718 , 0.00125978])},
  {'loss_function':weighted_spread_loss,'network':SegCaps_multilabels,
         'routing_type':'','batch_size':1,'max_iter':7500,
